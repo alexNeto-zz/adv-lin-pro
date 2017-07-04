548cab7235200b84c1f04f100087c892492cb94a
alexNeto <alex.neto3@hotmail.com>
2017-07-04 14:33:59 +0000

    add more to thread section

---
+++ THREAD/detached.c
@@ -0,0 +1,23 @@
+/* Skelton program that creates a detached thread */
+#include <pthread.h>
+
+void *thread_function(void *thread_arg){
+  // Do work here
+}
+
+int main(){
+
+  pthread_attr_t attr;
+  pthread_t thread;
+
+  pthread_attr_init (&attr);
+  pthread_attr_setdetachedstate(&attr, PTHREAD_CREATE_DETACHED);
+  pthread_create(&thread, &attr, &thread_function, NULL);
+  pthread_attr_destroy(&attr);
+
+  // Do work here
+
+  // No need to join the second thread 
+
+  return 0;
+}
